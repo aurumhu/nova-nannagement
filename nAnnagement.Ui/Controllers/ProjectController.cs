7fd9e80884e1b1fdcda590fcff7abd121e385003
aurumhu <aurumhu@users.noreply.github.com>
2017-03-10 17:08:34 -0400

    Project controller (#3)
    * Projects module setup
    
    * NuGet packages added to project

---
+++ nAnnagement.Ui/Controllers/ProjectController.cs
@@ -0,0 +1,39 @@
+﻿namespace nAnnagement.Ui.Controllers
+{
+    using Models;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    [Authorize]
+    public class ProjectController : Controller
+    {
+        public ProjectController()
+        {
+
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(ProjectDto project)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
